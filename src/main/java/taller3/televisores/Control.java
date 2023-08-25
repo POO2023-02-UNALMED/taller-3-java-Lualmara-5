8116a642e4f1479e0c64a70cacacc56b42950e12
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-25 17:56:47 -0500

    Class General (Error 4)😿

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,51 +1,42 @@
 package taller3.televisores;
 
 public class Control {
-	
-	private TV tv;
-	
-	public void enlazar(TV tv) {
-		this.tv = tv;
-		this.tv.setControl(this);
-	}
-	
-	public void setTv(TV tv) {
-		this.tv = tv;
-	}
-	
-	public TV getTv() {
-		return tv;
-	}
-	
-	public void setCanal(int canal) {
-		this.tv.setCanal(canal);
-	}
-	
-	public void setVolumen(int volumen) {
-		this.tv.setVolumen(volumen);
-	}
-	
-	public void volumenDown() {
-		this.tv.volumenDown();
-	}
-	
-	public void volumenUp() {
-		this.tv.volumenUp();
-	}
-	
-	public void canalDown() {
-		this.tv.canalDown();
-	}
-	
-	public void canalUp() {
-		this.tv.canalUp();
-	}
-	
-	public void turnOff() {
-		this.tv.setEstado(false);
-	}
-	
-	public void turnOn() {
-		this.tv.setEstado(true);
-	}
+    private TV tv;
+
+    public void turnOn(){
+        tv.turnOn();
+    }
+    public void turnOff(){
+        tv.turnOff();
+    }
+
+    public void canalUp(){
+        tv.canalUp();
+    }
+    public void canalDown(){
+        tv.canalDown();
+    }
+
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+
+    public void setCanal(int canal){
+        tv.setCanal(canal);
+    }
+
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public TV getTv(){
+        return this.tv;
+    }
+    public void setTv(TV tv){
+        this.tv = tv;
+    }
 }
