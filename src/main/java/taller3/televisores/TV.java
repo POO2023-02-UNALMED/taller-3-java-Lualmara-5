0aa6795c979ababaa981a80c533a2aec2d2d0f8b
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-25 16:14:24 -0500

    Class TV part 1

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -2,4 +2,14 @@ package taller3.televisores;
 
 public class TV {
 
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+
+    private static int numTV;
+
+    		
 }
