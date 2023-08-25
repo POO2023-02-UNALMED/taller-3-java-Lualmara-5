c8b45e12c889d65b18f85cd94a8814cfb0805e64
Lualmara-5 <luimartinezra@unal.edu.co>
2023-08-25 17:23:40 -0500

    Class TV Part 3
    😸

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -11,5 +11,101 @@ public class TV {
 
     private static int numTV;
 
-    		
+    public TV (Marca marca, boolean estado){
+        this.marca = marca;
+        this.estado = estado;
+        TV.numTV++;
+    }
+
+    public Marca getMarca(){
+        return this.marca;
+    }
+    public void setMarca(Marca marca){
+        this.marca = marca;
+    }
+
+    public Control getControl(){
+        return this.control;
+    }
+    public void setControl(Control control){
+        this.control = control;
+    }
+
+    public int getPrecio(){
+        return this.precio;
+    }
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public int getVolumen(){
+        return this.volumen;
+    }
+    public void setVolumen(int volumen){
+        if (this.estado == true){
+            if (volumen >= 0 && volumen <=7){
+                this.volumen = volumen;
+            }
+        }
+    }
+    
+    public int getCanal(){
+        return this.canal;
+    }
+    public void setCanal(int canal){
+        if (this.estado == true){
+            if (canal >= 1 && canal <= 120){
+                this.canal = canal;
+            }
+        }
+    }
+
+    public static int getNumTV(){
+        return numTV;
+    }
+    public static void setNumTV(int numTV){
+        TV.numTV = numTV;
+    }
+
+    public void turnOn() {
+        this.estado = true;
+    }
+    public void turnOff(){
+        this.estado = false;
+    }
+
+    public boolean getEstado(){
+        return this.estado;
+    }
+
+    public void canalUp(){
+        if (this.estado == true){
+            if (this.canal >= 1 && this.canal < 120){
+                this.canal++;
+            }
+        }
+    }
+    
+    public void canalDown(){
+        if (this.estado == true){
+            if (this.canal > 1 && this.canal <= 120){
+                this.canal--;
+            }
+        }
+    }
+
+    public void volumenUp(){
+        if (this.estado == true){
+            if (this.volumen >= 0 && this.volumen < 7){
+                this.volumen++;
+            }
+        }
+    }
+    public void volumenDown(){
+        if (this.estado == true){
+            if (this.volumen > 0 && this.volumen <= 7){
+                this.canal--;
+            }
+        }
+    }	
 }
